620c6880e4d942da4db1aa8eb1513b0b235e460b
Jose Ramos <jramoscolon@yahoo.com>
2016-11-11 23:36:29 -0400

    added descending order show view

--- app/controllers/episodes_controller.rb
+++ app/controllers/episodes_controller.rb
@@ -25,6 +25,7 @@ class EpisodesController < ApplicationController
   # GET /episodes/1
   # GET /episodes/1.json
   def show
+    @episodes = Episode.order("created_at DESC")
   end
 
   # GET /episodes/new
