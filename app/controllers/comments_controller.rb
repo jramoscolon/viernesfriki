2e44904ea29d09c60dd346b3568ec63ad78daad6
Jose Ramos <jramoscolon@yahoo.com>
2017-01-28 12:54:36 -0400

    added admin to comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -18,6 +18,10 @@ class CommentsController < ApplicationController
 	end
 
 	def destroy
+		@comment = Comment.find(params[:id])
+		episode = @comment.episode
+		@comment.destroy
+		redirect_to episode
 	end
 
 	private
